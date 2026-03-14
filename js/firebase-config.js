8570cbc80826cae967ed090f5bdf8c83a1926493
Thiago <thiagod11lopes@gmail.com>
2026-03-14 18:12:24 -0300

    Atualizacao Vitrine Net

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,11 +9,11 @@
  * Enquanto estiver com SUA_API_KEY, o app usa localStorage.
  */
 var FIREBASE_CONFIG = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto-id",
-  storageBucket: "seu-projeto.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxx",
-  measurementId: "G-XXXXX"
+  apiKey: "AIzaSyD_fmghbt3bt0hlI_baScShesW-EuZkYhg",
+  authDomain: "afiliado-87d75.firebaseapp.com",
+  projectId: "afiliado-87d75",
+  storageBucket: "afiliado-87d75.firebasestorage.app",
+  messagingSenderId: "772886912797",
+  appId: "1:772886912797:web:5c6dfee870769dec274bf4",
+  measurementId: "G-EYC31EV3JY"
 };
